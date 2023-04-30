5e1813e18e700e86d352fedf3ecf729107575161
Ryan Wilkie <r.wilkie@daxtra.com>
2023-04-30 22:31:01 +0100

    day one, part two

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,39 @@
-use std::collections::HashMap;
-
 fn main() {
-    day_one_one();
+    day_one(3);
 }
 
-fn day_one_one() {
-    let mut elves: HashMap<i32, i32> = HashMap::new();
+fn day_one(top_elf_count: i32) {
+    let mut elves = Vec::new();
     let file = std::fs::read_to_string("src/inputs/day_one").unwrap();
 
-    let mut counter = 0;
+    elves.push(0);
     file.lines().for_each(|x| {
         if x == "" {
-            counter += 1;
+            elves.push(0);
         } else {
             let num_x = match x.parse::<i32>() {
                 Ok(number) => number,
                 Err(..) => 0,
             };
-            let curr_val = match elves.get(&counter) {
-                Some(val) => val,
-                None => &0,
+            let curr_val = match elves.pop() {
+                Some(number) => number,
+                None => 0,
             };
 
-            elves.insert(counter, num_x + curr_val);
+            elves.push(num_x + curr_val);
         }
     });
 
-    let mut most_calorific_elf = (-1, -1);
+    elves.sort();
 
-    for (key, value) in &elves {
-        if value > &most_calorific_elf.1 {
-            most_calorific_elf = (*key, *value);
-        }
+    let mut total_calories = 0;
+    for _n in 1..=top_elf_count {
+        let val = match elves.pop() {
+            Some(number) => number,
+            None => 0,
+        };
+        total_calories += val;
     }
 
-    println!("The highest calories is {}", most_calorific_elf.1);
+    println!("total calories {}", total_calories);
 }
